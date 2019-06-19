6ef6bd0afdac7a877b8b53053a1d494bb50c1ff0
Lars Schiller <lars@familysc.de>
2019-06-19 19:17:08 +0200

    some plots.. jacobian

--- Jacobi_determination.py
+++ Jacobi_determination.py
@@ -8,7 +8,7 @@ Created on Tue Jun 18 17:53:17 2019
 from sympy import symbols, exp, I, sin, cos
 import sympy
 
-sympy.init_printing()
+#sympy.init_printing()
 
 
 a0, a1, a2 = symbols('a_0 a_1 a_2', real=True)
@@ -23,42 +23,143 @@ c0, c1, c2, c3 = symbols('c_0 c_1 c_2 c_3', real=True)
 #    return (-.005*x1 - 10.85*x2 - 2.55*x2**2 - .835*x1*x2)
 
 
-def dx(x1, x2):
-    return [a0*x1 + a1*x2 + a2*x2**2,
-            b0*x2 + b1*x2**2 + b2*x1*x2]
+#def dx(x1, x2):
+#    return [a0*x1 + a1*x2 + a2*x2**2,
+#            b0*x2 + b1*x2**2 + b2*x1*x2]
+#
+#
+#def deps(x1, x2):
+#    return (c0*x1 + c1*x2 + c2*x2**2 + c3*x1*x2)
 
+
+def dx(x1, x2):
+    return [.02*x1 + .13*x2 - .47*x2**2,
+            -(.07*x2 - .29*x2**2 + .02*x1*x2)]
 
 def deps(x1, x2):
-    return (c0*x1 + c1*x2 + c2*x2**2 + c3*x1*x2)
+    return (-.005*x1 - 10.85*x2 - 2.55*x2**2 - .835*x1*x2)
+
+
+def sumsin(x, n):
+    return 1/sin(x/2)*sin((n+1)*x/2)*sin(n*x/2)
+
+def sumcos(x, n):
+    return 1/sin(x/2)*sin((n+1)*x/2)*cos(n*x/2)
+
+def Esumsin(x, n):
+    return (2*I)/(exp(I*x/2)-exp(-I*x/2)) * ((exp(I*((n+1)*x)/2) - exp(-I*((n+1)*x)/2))/(2*I)) * ((exp(I*(n*x/2)) - exp(-I*(n*x/2)) )/(2*I))
+
+def Esumcos(x, n):
+    return (2*I)/(exp(I*x/2)-exp(-I*x/2)) * ((exp(I*((n+1)*x)/2) - exp(-I*((n+1)*x)/2))/(2*I)) * ((exp(I*(n*x/2)) + exp(-I*(n*x/2)) )/(2))
+
+def R(alp):
+    return [[cos(alp), -sin(alp)],
+            [sin(alp), cos(alp)]]
+
+def Esin(alp):
+    return (exp(I*alp) - exp(-I*alp))/(2*I)
+
+
+def Ecos(alp):
+    return (exp(I*alp) + exp(-I*alp))/(2)
+
+
+def ER(alp):
+    return [[Ecos(alp), -Esin(alp)],
+            [Esin(alp), Ecos(alp)]]
+
+
+def sumR(alp, n):
+    return [[sumcos(alp, n), -sumsin(alp, n)],
+            [sumsin(alp, n), sumcos(alp, n)]]
+
+def EsumR(alp, n):
+    return [[Esumcos(alp, n), -Esumsin(alp, n)],
+            [Esumsin(alp, n), Esumcos(alp, n)]]
+
+
+def multiply(A, x):
+    a11 = A[0][0]
+    a12 = A[0][1]
+    a21 = A[1][0]
+    a22 = A[1][1]
+    return [a11*x[0] + a12*x[1], a21*x[0] + a22*x[1]]
+
+
+
+def calc_d(xbar, dx, deps, n):
+    val1 = multiply(R(-n*deps), xbar)
+    val2 = multiply(sumR(-deps, n), dx)
+    dist_vec_x = val1[0] - val2[0]
+    dist_vec_y = val1[1] - val2[1]
+    return dist_vec_x**2 + dist_vec_y**2
+
+
+def Ecalc_d(xbar, dx, deps, n):
+    val1 = multiply(ER(-n*deps), xbar)
+    val2 = multiply(EsumR(-deps, n), dx)
+    dist_vec_x = val1[0] - val2[0]
+    dist_vec_y = val1[1] - val2[1]
+    return dist_vec_x**2 + dist_vec_y**2
+
+
 
 
 x1 = symbols('x1', real=True)
 x2 = symbols('x2', real=True)
 n = symbols('n', real=True)
+#n = 2
 
 
+
+
+# %% Euler sum Sin diff
 #x = symbols('delta_varepsilon', real=True)
 x = deps(x1, x2)
 
 
-sumsin = (2*I)/(exp(I*x/2)-exp(-I*x/2)) * ((exp(I*((n+1)*x)/2) - exp(-I*((n+1)*x)/2))/(2*I)) * ((exp(I*(n*x/2)) - exp(-I*(n*x/2)) )/(2*I))
+#sumsin = (2*I)/(exp(I*x/2)-exp(-I*x/2)) * ((exp(I*((n+1)*x)/2) - exp(-I*((n+1)*x)/2))/(2*I)) * ((exp(I*(n*x/2)) - exp(-I*(n*x/2)) )/(2*I))
+#print('sumsin1')
+#sympy.print_latex(sympy.simplify(sumsin))
+#print('diff sumsin1 dx1')
+#sympy.print_latex(sympy.simplify(sympy.diff(sumsin, x1)))
+#print('diff sumsin1 dx2')
+#sympy.print_latex(sympy.simplify(sympy.diff(sumsin, x2)))
 
-print('sumsin1')
-sympy.print_latex(sympy.simplify(sumsin))
-print('diff sumsin1 dx1')
-sympy.print_latex(sympy.simplify(sympy.diff(sumsin, x1)))
-print('diff sumsin1 dx2')
-sympy.print_latex(sympy.simplify(sympy.diff(sumsin, x2)))
+
+# %% Normal SumSin Diff
+#sumsin2 = 1/sin(x/2)*sin((n+1)*x/2)*sin(n*x/2)
+#sumsin2 = sympy.simplify(sumsin2)
+#
+#print('sumsin2')
+#sympy.print_latex(sympy.simplify(sumsin2))
+#
+#print('diff sumsin2 dx1')
+#sympy.print_latex(sympy.simplify(sympy.diff(sumsin2, x1)))
+#print('diff sumsin2 dx2')
+#sympy.print_latex(sympy.simplify(sympy.diff(sumsin2, x2)))
+
+# %% Sym Diff d
+xbarx = symbols('xb_x', real=True)
+xbary = symbols('xb_y', real=True)
+xbar = [xbarx, xbary]
+
+# %% Distance Euler Formulation
+
+d = Ecalc_d(xbar, dx(x1, x2), deps(x1, x2), n)
+
+print('Distance to goal -- Euler')
+sympy.print_latex(sympy.simplify(d))
 
 
-sumsin2 = 1/sin(x/2)*sin((n+1)*x/2)*sin(n*x/2)
-sumsin2 = sympy.simplify(sumsin2)
+# %% Distance
 
-print('sumsin2')
-sympy.print_latex(sympy.simplify(sumsin2))
+deps_dummy = symbols('delta_varepsilon', real=True)
+d = calc_d(xbar, dx(x1, x2), deps_dummy, n)
 
-print('diff sumsin2 dx1')
-sympy.print_latex(sympy.simplify(sympy.diff(sumsin2, x1)))
-print('diff sumsin2 dx2')
-sympy.print_latex(sympy.simplify(sympy.diff(sumsin2, x2)))
+print('Distance to goal')
+sympy.print_latex(d)
 
+# %% Simplify
+#print('Distance to goal - symplified')
+#sympy.print_latex(sympy.simplify(d))
--- Jacobi_determination_plots.py
+++ Jacobi_determination_plots.py
@@ -12,28 +12,51 @@ from matplotlib import cm
 
 
 def dx(x1, x2):
-    return [.02*x1 + .13*x2 - .47*x2**2,
-            -(.07*x2 - .29*x2**2 + .02*x1*x2)]
+    return np.array([
+            [.02*x1 + .13*x2 - .47*x2**2],
+            [-(.07*x2 - .29*x2**2 + .02*x1*x2)]
+            ])
 
 
 def deps(x1, x2):
-    return (-.005*x1 - 10.85*x2 - 2.55*x2**2 - .835*x1*x2)
+    return np.deg2rad(-.005*x1 - 10.85*x2 - 2.55*x2**2 - .835*x1*x2)
 
 
 def sumsin(x, n):
     return 1/np.sin(x/2)*np.sin((n+1)*x/2)*np.sin(n*x/2)
 
+def sumcos(x, n):
+    return 1/np.sin(x/2)*np.sin((n+1)*x/2)*np.cos(n*x/2)
+
+
+def R(alp):
+    return np.array(
+            [[np.cos(alp), -np.sin(alp)],
+             [np.sin(alp), np.cos(alp)]
+             ])
+
+def sumR(alp, n):
+    return np.array(
+            [[sumcos(alp, n), -sumsin(alp, n)],
+             [sumsin(alp, n), sumcos(alp, n)]
+             ])
+
+
+def calc_d(xbar, dx, deps, n):
+    return np.linalg.norm(
+            R(-n*deps)*xbar - sumR(-deps, n)*dx)
+
+
+
+# %% Plot SumSin
 n = np.arange(4, 10, 1)
-
 xdeps = np.arange(-4*np.pi, 4*np.pi, .01)
-
-
-
-
 SUMSIN = np.zeros((len(xdeps), len(n)))
+
 for nidx, ni in enumerate(n):
     for didx, depsi in enumerate(xdeps):
         SUMSIN[didx][nidx] = sumsin(depsi, ni)
+
 
 
 N, DEPS = np.meshgrid(n, xdeps)
@@ -51,4 +74,28 @@ ax.set_ylabel('deps')
 ax.set_zlabel('sumsin')
 
 fig.set_size_inches(18.5, 10.5)
+
+
+# %% Plot D(x1, x2)
+def plot_contour(X1, X2, D):
+    X1, X2 = np.meshgrid(X1, X2)
+    fig, ax = plt.subplots()
+    cs = ax.contourf(X1, X2, D)
+    cs = ax.contour(X1, X2, D, 5, colors='k')
+    ax.clabel(cs, inline=1, fontsize=10)
+
+
+
+
+xbar = np.array([[3], [0]])
+X1 = np.arange(0, 90, 2)
+X2 = np.arange(-.5, .5, .01)
+D = np.zeros((len(X2), len(X1)))
+n = 2
+for idx1, x1 in enumerate(X1):
+    for idx2, x2 in enumerate(X2):
+        D[idx2][idx1] = calc_d(xbar, dx(x1, x2), deps(x1, x2), n)
+
+plot_contour(X1, X2, D)
+
 
