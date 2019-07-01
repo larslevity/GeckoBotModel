15d0d5a9885a3d082317d9fe14cd913c1222ff5b
Lars Schiller (AmP) <lars@familysc.de>
2019-07-01 15:28:51 +0200

    workspace study finis

--- workspace_study.py
+++ workspace_study.py
@@ -36,19 +36,45 @@ if __name__ == "__main__":
             for gam in np.arange(0,91,step):
                 refs.append([[0, alp, bet, gam, 0], [0, 1, 1, 0]])
 
-    for ref in refs:
+    maxima = {'minx': 1, 'maxx': 1, 'miny': 1, 'maxy': 1}
+    index = {'minx': 0, 'maxx': 0, 'miny': 0, 'maxy': 0}
+    for idx, ref in enumerate(refs):
 
         x, marks, f, constraint, cost = model.predict_next_pose(
             ref, x, marks, len_leg=ell[0], len_tor=ell[2])
         gait.append_pose(
             pf.GeckoBotPose(x, marks, f, constraint=constraint, cost=cost))
+        # check maxima:
+        px, py = marks[0][1], marks[1][1]
+        if px < maxima['minx']:
+            maxima['minx'] = px
+            index['minx'] = idx
+        if px > maxima['maxx']:
+            maxima['maxx'] = px
+            index['maxx'] = idx
+        if py < maxima['miny']:
+            maxima['miny'] = py
+            index['miny'] = idx
+        if py > maxima['maxy']:
+            maxima['maxy'] = py
+            index['maxy'] = idx
+        
 
 
 #%%
 #    gait.plot_gait()
     gait.plot_markers([1])
-#    gait_str = gait.get_tikz_repr()
-    save.save_plt_as_tikz('Out/workspace/test.tex')
+    plt.axis('off')
+    gait_str = gait.poses[index['minx']].get_tikz_repr()
+    gait_str = gait_str + gait.poses[index['maxx']].get_tikz_repr()
+    gait_str = gait_str + gait.poses[index['miny']].get_tikz_repr()
+    gait_str = gait_str + gait.poses[index['maxy']].get_tikz_repr()
+    
+    gait.poses[index['minx']].show_stats()
+    gait.poses[index['maxx']].show_stats()
+    gait.poses[index['maxy']].show_stats()
+    
+    save.save_plt_as_tikz('Out/workspace/test.tex', gait_str)
 
 
 # %%
