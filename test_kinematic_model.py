7aab9d07f36519c3d136def993db88511b8f368f
Lars Schiller (T560) <lars.schiller@tuhh.de>
2020-05-04 20:26:35 +0200

    ref test_kinematic_model.py

--- test_kinematic_model.py
+++ test_kinematic_model.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     from Src.Utils import plot_fun as pf
     from Src.Math import kinematic_model as model
 
-    alp = [90, 0, -90, 90, 0]
-    eps = 90
-    ell = [1, 1, 1.2, 1, 1]
+    alp = [5, 110, 120, 10, 110]
+    eps = 20
+    ell = [3, 3, 3.5, 3, 3]
     p1 = (5, 10)
 
     x, marks, f = model.set_initial_pose(
@@ -25,19 +25,19 @@ if __name__ == "__main__":
     gait.append_pose(initial_pose)
 
     # REF
-    ref1 = [[20, 90, 90, 0, 120], [0, 1, 1, 0]]
-    ref2 = [[100, 10, -70, 80, 2], [1, 0, 0, 1]]
+    ref1 = [[70, 0, -45, 70, 0], [1, 0, 0, 1]]
+    ref2 = [alp, [0, 1, 1, 0]]
 
     tic = time.time()
     x, marks, f, constraint, cost = model.predict_next_pose(
-        ref1, x, marks, len_leg=1, len_tor=1.2)
+        ref1, x, marks, len_leg=ell[0], len_tor=ell[2])
     print('ellapsed time:', time.time()-tic, 's')
     gait.append_pose(
         pf.GeckoBotPose(x, marks, f, constraint=constraint, cost=cost))
 
     tic = time.time()
     x, marks, f, constraint, cost = model.predict_next_pose(
-        ref2, x, marks, len_leg=1, len_tor=1.2)
+        ref2, x, marks, len_leg=ell[0], len_tor=ell[2])
     print('ellapsed time:', time.time()-tic, 's')
     gait.append_pose(
         pf.GeckoBotPose(x, marks, f, constraint=constraint, cost=cost))
