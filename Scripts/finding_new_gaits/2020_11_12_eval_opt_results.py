a4c780959a55b24d5f60a185817242a49a9bd751
Lars Schiller (T560) <lars.schiller@tuhh.de>
2020-11-15 20:06:24 +0100

    RAL redo eval

--- Scripts/finding_new_gaits/2020_11_12_eval_opt_results.py
+++ Scripts/finding_new_gaits/2020_11_12_eval_opt_results.py
@@ -9,6 +9,7 @@ Created on Thu Nov 12 17:33:27 2020
 import pickle
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as matcols
@@ -26,39 +27,13 @@ RESULTS = load_obj('RESULTS_curve')
 
 
 
-
-def plot3dScatter(arr, cmap, F, name=''):
-    fig = plt.figure(name)
-    ax = fig.add_subplot(111, projection='3d')
-    minval = np.min(arr)
-    maxval = np.max(arr)
-    
-    for iori in range(len(arr)):
-        for iang in range(len(arr[0])):
-            for ilen in range(len(arr[0][0])):
-                val = arr[iori][iang][ilen]
-                [f_len, f_ang, f_ori] = np.log10(F[ilen][iang][iori])
-                
-                
-                
-                normval = (val-minval)/maxval
-                col = cmap(normval)
-
-                ax.scatter(f_len, f_ang, f_ori, marker='o', color=col,
-                           s=5+normval*20)
-#                ax.text(f_len, f_ang, f_ori, '%2.1f' % val, 'x')
-   
-    ax.set_xlabel('$f_l$')
-    ax.set_ylabel('$f_a$')
-    ax.set_zlabel('$f_o$')
-    norm = matcols.Normalize(vmin=minval, vmax=maxval, clip=False)
-    plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
-
-
-
 def plot2dScatter(arr, cmap, F, name='', markers='linear' , **kwargs):
+    
+
     fig = plt.figure(name)
-    ax = fig.add_subplot(111)
+    (ax, cax) = fig.subplots(1, 2, gridspec_kw={'width_ratios': [95, 5]})
+#    ax = fig.add_subplot(121)
+#    cax = fig.add_subplot(122, aspect=0.3)
     minval = np.min(arr)
     maxval = np.max(arr)
     
@@ -87,10 +62,11 @@ def plot2dScatter(arr, cmap, F, name='', markers='linear' , **kwargs):
                          color=col, markersize=size, **kwargs)
                 if iori == 0 and iang == 0 and ilen in [0, N_flen-1]:
                     ax.annotate('$10^{%d}$' % f_len10, 
-                                (10**(f_ori10 +f_len10*zscale-.2), 10**(f_ang10+f_len10*zscale+.1)))
+                                (10**(f_ori10 +f_len10*zscale-.2), 10**(f_ang10+f_len10*zscale+.1)),
+                                fontsize=20)
 
-    ax.annotate('$w_{\\ell}$' % f_len10, 
-                (10**(-2-zscale+.2), 10**(-2-zscale-.2)))
+    ax.annotate('$w_{\\ell}~(1)$' % f_len10, 
+                (10**(-2-zscale+.2), 10**(-2-zscale-.2)), fontsize=20)
         
     ax.annotate('', xytext=(10**(-2 +-2*zscale), 10**(-2+-2*zscale-.2)), 
                 xy=(10**(-2 +3*zscale), 10**(-2+3*zscale-.2)),
@@ -100,21 +76,39 @@ def plot2dScatter(arr, cmap, F, name='', markers='linear' , **kwargs):
                     
 #                ax.text(f_len, f_ang, f_ori, '%2.1f' % val, 'x')
 
-    ax.set_xlabel('$w_{\\varphi}$')
-    ax.set_ylabel('$w_{\\alpha}$')
+    ax.set_xlabel('$w_{\\varphi}~(1)$')
+    ax.set_ylabel('$w_{\\alpha}~(1)$')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    norm = matcols.Normalize(vmin=minval, vmax=maxval, clip=False)
-    bar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
-    bar.set_label(name)
+    ax.grid('on')
+    
+    # color bar hack to export as tikz
+    x = np.linspace(0, 1, 2)
+    y = np.linspace(minval, maxval, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = Y
+    cax.contourf(X, Y, Z, cmap=cmap, levels=100)
+    cax.yaxis.tick_right()
+    cax.yaxis.set_label_position("right")
+    cax.set_ylabel(name)
+    
+    cax.set_xticklabels([' '], color='white')
+    cax.set_xticks([0])
+    cax.xaxis.label.set_color('white')
+    cax.tick_params(axis='x', colors='white')
+
+
+    cax.grid('off')     
+
+#    im = cax.imshow(np.linspace(1,0,100).reshape((100, 1)))
+#    
+#    norm = matcols.Normalize(vmin=minval, vmax=maxval, clip=False)
+#    bar = plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=cax,
+#                       orientation="vertical")
+#    bar.set_label(name)
     plt.grid()
     fig.tight_layout()
     return fig
-
-
-
-              
-
 
 
 
@@ -144,7 +138,7 @@ for method in ['COBYLA']:
                     STRESS[iori][iang][ilen] = stress
                     DX[iori][iang][ilen] = abs(dist[0])  # obacht!
                     DY[iori][iang][ilen] = dist[1]
-                    DEPS[iori][iang][ilen] = abs(deps)
+                    DEPS[iori][iang][ilen] = abs(deps) # obacht!
                     
                     alp_opt = RESULTS[method][cat][f_len][f_ang][f_ori]['alp']
 
@@ -154,17 +148,26 @@ for method in ['COBYLA']:
         jet = plt.get_cmap('jet')
         coolwarm = plt.get_cmap('coolwarm')
         #%%
-        fig = plot2dScatter(DY, coolwarm, F, '$\\Delta y$',
-                      markers='linear', marker='o')
+        fig = plot2dScatter(DY, jet, F, '$\\Delta y$', markers='linear', marker='o')
         fig.savefig('Out/'+method+'_'+cat+'_DY.pdf')
+        tikzplotlib.save('Out/tex/'+method+'_'+cat+'_DY.tex', standalone=True,
+                         extra_axis_parameters={'width=15cm', 'height=8cm'})
         
-
+        
         fig=plot2dScatter(DX, ampel_rev, F, '$\\Delta x$', markers='exponetial_decrease', marker='o')
         fig.savefig('Out/'+method+'_'+cat+'_DX.pdf')
+        tikzplotlib.save('Out/tex/'+method+'_'+cat+'_DX.tex', standalone=True,
+                         extra_axis_parameters={'width=15cm', 'height=8cm'})
+
         fig=plot2dScatter(STRESS, ampel_rev, F, 'stress', markers='decrease', marker='o')
         fig.savefig('Out/'+method+'_'+cat+'_STRESS.pdf')
-        fig=plot2dScatter(DEPS, ampel_rev, F, '$\\Delta \\varepsilon$', markers='exponetial_decrease', marker='o')
+        tikzplotlib.save('Out/tex/'+method+'_'+cat+'_STRESS.tex', standalone=True,
+                         extra_axis_parameters={'width=15cm', 'height=8cm'})
+
+        fig=plot2dScatter(DEPS, jet, F, '$|\\Delta \\varepsilon|$', markers='linear', marker='o')
         fig.savefig('Out/'+method+'_'+cat+'_DEPS.pdf')
+        tikzplotlib.save('Out/tex/'+method+'_'+cat+'_DEPS.tex', standalone=True,
+                         extra_axis_parameters={'width=15cm', 'height=8cm'})
 
 #        plot3dScatter(DEPS, ampel_rev, F, 'Delta epsilon')
         
